af2ad87eb6915c376eaa3e3edba7fc00806ebc3e
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-11 10:20:56 -0300

    Update useDebounce.ts

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,19 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import useDebounce from '../hooks/useDebounce';
+import { UserInfo } from '../types';
 
-function useDebounce<T>(value: T, delay: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
+// Props esperadas (exemplo)
+interface PersonalInfoProps {
+  initialUserInfo: UserInfo;
+  onSave: (userInfo: UserInfo) => void;
 }
 
-export default useDebounce;
+const PersonalInfo: React.FC<PersonalInfoProps> = ({ initialUserInfo, onSave }) => {
+  const [userInfo, setUserInfo] = useState<UserInfo>(initialUserInfo);
+
+  // Debounce para evitar múltiplos saves muito rápidos
+  const debouncedUserInfo = useDebounce(userInfo, 500);
+
+  // Quando debouncedUserInfo muda, chama onSave
+  useEffect(() => {
+    onSave(debouncedUserInfo);
+  }, [debouncedUserInfo, onSave]);
+
+  // Exemplo simples de campo de input para nome
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setUserInfo(prev => ({
+      ...prev,
+      name: e.target.value,
+    }));
+  }
+
+  return (
+    <div>
+      <label htmlFor="name">Nome:</label>
+      <input
+        id="name"
+        type="text"
+        value={userInfo.name || ''}
+        onChange={handleChange}
+      />
+      {/* Aqui você pode adicionar outros campos como peso, altura, etc */}
+    </div>
+  );
+};
+
+export default PersonalInfo;
