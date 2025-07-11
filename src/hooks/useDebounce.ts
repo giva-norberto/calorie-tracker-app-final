67cd44eef52b3030bf0607a22235bdfb3219a7ea
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2025-07-11 10:11:10 -0300

    Update useDebounce.ts

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import useDebounce from '../hooks/useDebounce';
 
 function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
